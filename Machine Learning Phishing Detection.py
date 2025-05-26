29fc94ab3772b7ad5ba9baac6e82683b7b455913
Prakanth20 <prakanthvijayakumar20@gmail.com>
2025-05-26 16:08:33 +0530

    Update Machine Learning Phishing Detection.py

--- Machine Learning Phishing Detection.py
+++ Machine Learning Phishing Detection.py
@@ -58,7 +58,7 @@ model.fit(X_train, y_train)
 # === Evaluate ===
 y_pred = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred, zero_division=0))
 
 # === Predict New URL ===
 def predict_url(url):
